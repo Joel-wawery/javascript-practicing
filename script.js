1014d2f08d28eed2f664b8ae2adc1461c1d2f544
Joel-wawery <163989746+Joel-wawery@users.noreply.github.com>
2025-11-12 09:59:42 +0300

    this code is teaching me switch statements

--- script.js
+++ script.js
@@ -1,27 +1,23 @@
-/*project to set up a login system for a website .here i need to check the status of the user and give them the 
-correct welcome message */
-//variables to set the value to be admin and userName
-const userRole = 'subscriber';
-const userName = 'Bob';
-//conditional loop to check if the admin role is set to admin role
-if (userRole === 'admin') {
-    //welcome the administrator
-    console.log('welcome administrator! you have full control');
-} else if (userRole === 'editor') {
-    //welcome message for the editor
-    console.log('welcome editor!you can write full content');
-} else if ( userRole === 'guest') {
-    //welcome the guest
-    console.log('welcome guest!! you have limited access points ');
-} else {
-    //this is an unknown user
-    console.log('unknown user role');
-}
-/*this code utilises the power of clean code of using truthy and falsy values  */
-if (userName) {
-    //logging the userName
-    console.log(userName);
-} else {
-    //logging the anonymous user
-    console.log('anonymous user detected');
+/*this code teaches us the use of switch statement and why its good to use it against a single variable */
+//variable command that is set to fetch
+let command = 'update';
+
+switch(command) {
+    case'fetch':
+         console.log('retrieving your data');
+         break;
+    case'save':
+        console.log('saving your data');
+        break;
+    case'delete':
+        console.log('deleting data');
+        break;
+    case'info':
+        console.log('displaying the information')
+        break;       
+        
+     default:
+        console.log('unknown user role');
+        break;   
+
 }
