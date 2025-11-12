c50172ea3c5032fc8de6e9dceceb29ff0528e1ca
Joel-wawery <163989746+Joel-wawery@users.noreply.github.com>
2025-11-12 09:48:19 +0300

    this code teaches me conditional operators

--- script.js
+++ script.js
@@ -1,33 +1,27 @@
-/*e-commerce checkout page */
-//variables to conatain the value of the total in the cart and status of the user
-let cartTotal = 91;
-let userStatus = 'premium';
-
-/*variable to contain the value that is the result of shipping cost and premiumMin */
-const shippingCost = 10;
-const premiumMin = 100;
-
-/*shipping cost is free for users in premium or for users with a cartTotal of over 100
-we set the values inside a boolean variable isShippingCost*/
-isShippingCost = userStatus==='premium' || cartTotal > premiumMin;
-console.log(isShippingCost);
-/*a new variable to finalTotal  and use of conditional logic*/
-if (isShippingCost === 'true') {
-    //this is the block of code that should run and shipping is free
-    finalTotal = cartTotal;
+/*project to set up a login system for a website .here i need to check the status of the user and give them the 
+correct welcome message */
+//variables to set the value to be admin and userName
+const userRole = 'subscriber';
+const userName = 'Bob';
+//conditional loop to check if the admin role is set to admin role
+if (userRole === 'admin') {
+    //welcome the administrator
+    console.log('welcome administrator! you have full control');
+} else if (userRole === 'editor') {
+    //welcome message for the editor
+    console.log('welcome editor!you can write full content');
+} else if ( userRole === 'guest') {
+    //welcome the guest
+    console.log('welcome guest!! you have limited access points ');
+} else {
+    //this is an unknown user
+    console.log('unknown user role');
 }
-else{
-    //this block of code runs and the shipping is not free so i add the shipping costs
-    finalTotal = cartTotal + shippingCost;
+/*this code utilises the power of clean code of using truthy and falsy values  */
+if (userName) {
+    //logging the userName
+    console.log(userName);
+} else {
+    //logging the anonymous user
+    console.log('anonymous user detected');
 }
-
-console.log(finalTotal)
-
-
-/*a user add an item so we use a assingment operator to add and assing cartTotal */
-cartTotal += 30;
-console.log(cartTotal);
-
-/*final checkif the userStatus is set to premuim */
-let isUserPremium = userStatus === 'premium';
-console.log('is the user premium ?' + isUserPremium);
